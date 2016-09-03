c17af2df702ab99a3b4da0f9b5ef7a454e7055fc
Jason Huang <jasson15@gmail.com>
2016-09-02 22:02:01 -0700

    Leveled panel

--- src/MessageBox.jsx
+++ src/MessageBox.jsx
@@ -7,17 +7,20 @@ var MessageList = require('./MessageList.jsx');
 var MessageForm = require('./MessageForm.jsx');
 
 import Chip from 'react-toolbox/lib/chip';
+import { Card, CardMedia, CardTitle, CardText, CardActions } from 'react-toolbox/lib/card';
 
 var MessageBox = React.createClass({
   render: () => (
-    <div style={{ flex: 1, overflowY: 'auto', padding: '1.8rem' }}>
-      <MessageHistory/>
-      <div style={{ justifyContent:'center', display: 'flex'}}>
-        <Chip>
-          ↑ <em>previous chats</em> ↑
-        </Chip>
-      </div>
-      <MessageList/>
+    <div>
+      <Card style={{ flex: 1, overflowY: 'auto', padding: '1.8rem' }}>
+        <MessageHistory/>
+        <div style={{ justifyContent:'center', display: 'flex'}}>
+          <Chip>
+            ↑ <em>previous chats</em> ↑
+          </Chip>
+        </div>
+        <MessageList/>
+      </Card>
       <MessageForm/>
     </div>
   )
